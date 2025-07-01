14492a2bc7fc7967e2421264b1d9f4e167ea014e
Fábio simones <simones.fabio1994@gmail.com>
2025-07-01 18:40:40 -0300

    Criando projeto.

---
+++ src/test/java/dev/fabiosimones/smartstock/SmartstockApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fabiosimones.smartstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
